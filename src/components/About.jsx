b9251b772f2601004e265b37d568bca7f8cf3d6c
PriyobrotoKarmakar <karmakarpriyobroto@gmail.com>
2025-08-24 02:04:30 +0530

    Update About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,9 +15,7 @@ const ServiceCard = ({ index, title, icon }) => (
       transition={{ duration: 0.8, delay: index * 0.2 }}
       className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
-      <div
-        className="bg-tertiary rounded-[20px] py-5 px-6 sm:px-12 min-h-[250px] sm:min-h-[280px] flex justify-evenly items-center flex-col"
-      >
+      <div className="bg-tertiary rounded-[20px] py-5 px-6 sm:px-12 min-h-[250px] sm:min-h-[280px] flex justify-evenly items-center flex-col">
         <img
           src={icon}
           alt="web-development"
@@ -50,11 +48,14 @@ const About = () => {
         transition={{ duration: 0.8, delay: 0.3 }}
         className="mt-4 text-secondary text-[15px] sm:text-[17px] max-w-3xl leading-[25px] sm:leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I`m a passionate software developer with expertise in C++, Python, Java,
+        and JavaScript, and hands-on experience with modern frameworks like
+        React.js, Flask, and the MERN stack. Skilled in building full-stack web
+        applications, AI-powered platforms, and scalable solutions deployed on
+        cloud services such as AWS and Vercel. I enjoy solving complex problems,
+        optimizing performance, and designing responsive, user-friendly
+        interfaces. A quick learner and strong collaborator, I thrive on turning
+        ideas into impactful, real-world projects.
       </motion.p>
 
       <div className="mt-10 sm:mt-20 flex flex-wrap justify-center gap-4 sm:gap-6">
